86b227bc06696a71dab2c25a78df77c0865f6ac9
dhairya3007 <12202080701030@mbit.edu.in>
2026-03-11 12:55:08 +0530

    Delete createsu.py management command
    Removed the createsu.py command for superuser creation.

--- store/management/commands/createsu.py
+++ store/management/commands/createsu.py
@@ -1,16 +1,1 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
 
-class Command(BaseCommand):
-    help = "Create superuser if not exists"
-
-    def handle(self, *args, **kwargs):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(
-                username="admin",
-                email="admin@example.com",
-                password="admin123"
-            )
-            self.stdout.write(self.style.SUCCESS("Superuser created"))
-        else:
-            self.stdout.write("Superuser already exists")
